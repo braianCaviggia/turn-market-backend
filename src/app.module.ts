45cdaf8fbbd7a02808fcb948586ea9b40240f124
braianCaviggia <braiancaviggia10@gmail.com>
2026-04-23 20:18:05 -0300

    fix database.config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { databaseConfig } from './config/database.config';
 import { UserModule } from './user/user.module';
 import { TurnModule } from './turn/turn.module';
 import { ProfessionalProfileModule } from './professional-profile/professional-profile.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(databaseConfig),
+    ConfigModule.forRoot({ isGlobal: true }), // Carga las variables
+    TypeOrmModule.forRootAsync(databaseConfig), // Usa la configuración asíncrona
     UserModule,
     TurnModule,
     ProfessionalProfileModule,
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,12 +1,15 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 
-export const databaseConfig: TypeOrmModuleOptions = {
+export const databaseConfig: TypeOrmModuleAsyncOptions = {
+  useFactory: (configService: ConfigService) => ({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'braian1491',
-  database: 'turn_market_db',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   autoLoadEntities: true,
   synchronize: true,
+  }),
 };
